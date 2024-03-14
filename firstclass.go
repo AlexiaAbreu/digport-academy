7755b8ef9db93d7cedf65841f9a9279d44afe196
AlexiaAbreu <alexia28.abreu@gmail.com>
2024-03-14 15:35:42 -0300

    aula dois - declaração de variáveis

--- firstclass.go
+++ firstclass.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// var nomeDoUsuario string
+
+	// fmt.Print("Digite seu nome:")
+	// fmt.Scanln(&nomeDoUsuario)
+	// fmt.Println("bem vinda", nomeDoUsuario)
+
+	var valor int // declaração
+	valor2 := 1   // atribuição
+
+	fmt.Printf("Digite um número: ")
+	fmt.Scanf("%d", &valor)
+	soma := valor + valor2
+
+	fmt.Println("soma é ", soma)
 }
